d323b17d72744beff8971b586020bbc423675740
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-09-01 18:56:44 +0200

    add server file and basic package.json dependencies

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running...');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.use((req, res) => {
+    res.status(404).send({ message: 'Not found...' });
+});
+
